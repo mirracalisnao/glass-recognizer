325b658492983ec511287e04a6207692e2c3b31d
Cherry Mirra Calisnao <99376788+mirracalisnao@users.noreply.github.com>
2024-02-24 20:18:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ from sklearn.metrics import classification_report
 def app():
     
     st.title('Symbol Classification')
-    st.subheader('by Louie F. Cervantes M.Eng., WVSU College of ICT')
+    st.subheader('by Cherry Mirra T. Calisnao  BSCS 3A')
     st.write('The naive bayes classifierperforms well on overlapped data.')
 
     st.write('Dataset description:')
@@ -43,7 +43,7 @@ def app():
         clf = BernoulliNB()
 
     if st.button('Start'):
-        df = pd.read_csv('smiley_faces.csv', header=None)
+        df = pd.read_csv('symbol.csv', header=None)
         # st.dataframe(df, use_container_width=True)  
         
         # display the dataset
