2e981857d86e894cf1f0989c74c8481b91453bef
Cherry Mirra Calisnao <99376788+mirracalisnao@users.noreply.github.com>
2024-02-27 11:41:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,8 @@ def app():
         Potential Applications:"""
     st.write(text)
     st.write('Digit recognition: Identifying handwritten digits from 0-9.')
-    st.write('Traffic sign classification: Classifying different types of traffic signs.')
-    st.write('Character recognition: Recognizing characters from different alphabets.')
-    st.write("""Simple image classification: Classifying simple images into categories 
-             like animal/non-animal, vehicle/non-vehicle, etc.""")
+    st.write('Glass recognition: Recognizing the different types of things that made up of glass.')
+    st.write("""Simple image classification: Classifying simple images into categories: Mug, Wine Glass, Test tube and Beaker.""")
 
     # display choice of classifier
     clf = BernoulliNB() 
